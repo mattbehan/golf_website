4147685e0870017d2ef5f75c11b86e4690a43e44
Matt Behan <mattbehan@gmail.com>
2017-07-23 14:18:55 -0400

    fixing score display 4

--- app/models/pool_participant.rb
+++ app/models/pool_participant.rb
@@ -16,12 +16,13 @@ class PoolParticipant < ActiveRecord::Base
 	# gets total score with respect to rules
 	def total_score
 		all_scores = picks.collect { |pick|
-			picks_golfers = TournamentGolfer.where(golfer_id: pick.golfer_id, tournament_id: pick.pool.tournament_id).pluck(:total)[0].to_i
+			picks_golfers = TournamentGolfer.where(golfer_id: pick.golfer_id, tournament_id: pick.pool.tournament_id).pluck(:total)[0]
 		} - [nil]
-		all_scores = all_scores.sort
 		if all_scores.count < 5
 			return "DQ"
 		else 
+			all_scores = all_scores.map {|score| score.to_i}
+			all_scores = all_scores.sort
 			return all_scores[0..(pool.number_golfers_for_scoring-1)].sum
 		end
 	end
