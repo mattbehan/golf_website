f1773477d8c5c32baf9867b59a75a873f41346c0
Matt Behan <mattbehan@gmail.com>
2018-06-11 21:27:52 -0400

    setting up mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+    ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'gmail.com'
+  }
 end
