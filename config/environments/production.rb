16950a0e56a19ad5622b4ee6b56416bcd280def5
Matt Behan <mattbehan@gmail.com>
2018-06-11 22:20:39 -0400

    email 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.action_mailer.default_url_options = { :host => 'https://thejacksonian.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.active_record.dump_schema_after_migration = false
     ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
